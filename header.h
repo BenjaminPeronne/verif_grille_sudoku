d0bbe3fbd765a71b038ece8631e6a2a453998a99
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-04-20 13:47:44 -0400

    Make `sudoku_print` function

--- header.h
+++ header.h
@@ -20,8 +20,22 @@ void sudoku_init(sudoku *s, int size) { // Initialize the sudoku grid with 0 (em
 }
 void sudoku_free(sudoku *s) {
 
-}
-void sudoku_print(sudoku *s){
+void sudoku_print(sudoku *s){ // Print the sudoku grid
+    printf("-------------------------\n");
+    for (int i = 0; i < s->size; i++) {
+        printf("| ");
+        for (int j = 0; j < s->size; j++) {
+            printf("%d ", s->grid[i][j]);
+            if ((j + 1) % 3 == 0) {
+                printf("| ");
+            }
+        }
+        printf("\n");
+        if (i == 2 || i == 5) {
+            printf("-------------------------\n");
+        }
+    }
+    printf("-------------------------\n");    
 
 }
 
